2f404755973b8bab6e4cdb70456541902c94639a
Nadia-Kadri <nadiagrace96@gmail.com>
2019-10-19 16:06:04 -0400

    update to html content; initial screen function; questionGenerator function

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,41 +1,67 @@
+let trivia = [
+
+    {question: "This is the first question",
+        answers: ["answer1", "answer2", "answer3", "answer4"],
+        rightAnswer: 1
+    },
+    
+    {question: "This is the second question",
+        answers: ["answer1", "answer2", "answer3", "answer4"],
+        rightAnswer: 1
+    },
+    
+    {question: "This is the third question",
+        answers: ["answer1", "answer2", "answer3", "answer4"],
+        rightAnswer: 1
+    },
+    
+    {question: "This is the fourth question",
+        answers: ["answer1", "answer2", "answer3", "answer4"],
+        rightAnswer: 1
+    }
+];
+
 let correctAnswers = 0;
 let incorrectAnswers = 0;
 let unAnswered = 0;
 
-let trivia = {
-    questionOne: {
-        question: "question",
-        rightAnswer: "right",
-        wrongAnswer: ["wrong1", "wrong2", "wrong3"]
-    },
-    questionTwo: {
-        question: "question",
-        rightAnswer: "right",
-        wrongAnswer: ["wrong1", "wrong2", "wrong3"]
-    },
-    questionThree: {
-        question: "question",
-        rightAnswer: "right",
-        wrongAnswer: ["wrong1", "wrong2", "wrong3"]
-    },
-    questionFour: {
-        question: "question",
-        rightAnswer: "right",
-        wrongAnswer: ["wrong1", "wrong2", "wrong3"]
+let gameStart = false;
+let currentQuestion = 0;
+
+$(document).ready(function () {
+
+    initialScreen ()
+
+    $("#start-button").on("click", function (){
+        gameStart = true;
+        questionGenerator ()
+    });
+
+
+    function initialScreen () {
+        $("#question").hide()
+        $(".answer").hide()
+        $("#timer").hide()
+        $(".display-right-answer").hide()
+        $(".score").hide()
     }
-};
 
-function reStart () {
-    $("<button>").text("start").appendTo("#button").click(startQuestions)
-}
+    function questionGenerator () {
+        $("#question").show().text(trivia[currentQuestion].question)
+        $(".answer").show()
+        $("#timer").show()
+        $("#game-rules").hide()
+        $("#start-button").hide()
+        
+        $("#answer-one").text(`a) ${trivia[currentQuestion].answers[0]}`)
+        $("#answer-two").text(`b) ${trivia[currentQuestion].answers[1]}`)
+        $("#answer-three").text(`c) ${trivia[currentQuestion].answers[2]}`)
+        $("#answer-four").text(`d) ${trivia[currentQuestion].answers[3]}`)
+    }
 
-reStart()
+});
 
 
-function startQuestions () {
-    console.log("hello")
-    
-}
 
 
 
