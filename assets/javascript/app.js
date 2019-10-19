2809e2bedb4b8295220232e8d6694c52f39b2b7d
Nadia-Kadri <nadiagrace96@gmail.com>
2019-10-19 17:51:08 -0400

    capture user guess, right answer function, wrong answer function

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,24 +1,20 @@
 let trivia = [
 
-    {question: "This is the first question",
-        answers: ["answer1", "answer2", "answer3", "answer4"],
+    {question: "What is Europe's most mountainous country?",
+        answers: ["Ireland", "Switzerland", "Belgium", "Sweden"],
         rightAnswer: 1
     },
     
-    {question: "This is the second question",
-        answers: ["answer1", "answer2", "answer3", "answer4"],
+    {question: "In what country is the Valley of the Kings located?",
+        answers: ["China", "Egypt", "Indonesia", "Australia"],
         rightAnswer: 1
     },
     
-    {question: "This is the third question",
-        answers: ["answer1", "answer2", "answer3", "answer4"],
+    {question: "What is the largest country in South America?",
+        answers: ["Argentina", "Brazil", "Peru", "Colombia"],
         rightAnswer: 1
     },
-    
-    {question: "This is the fourth question",
-        answers: ["answer1", "answer2", "answer3", "answer4"],
-        rightAnswer: 1
-    }
+
 ];
 
 let correctAnswers = 0;
@@ -27,6 +23,8 @@ let unAnswered = 0;
 
 let gameStart = false;
 let currentQuestion = 0;
+let counter = 5;
+let intervalId;
 
 $(document).ready(function () {
 
@@ -35,6 +33,7 @@ $(document).ready(function () {
     $("#start-button").on("click", function (){
         gameStart = true;
         questionGenerator ()
+        generateTimer ()
     });
 
 
@@ -43,6 +42,8 @@ $(document).ready(function () {
         $(".answer").hide()
         $("#timer").hide()
         $(".display-right-answer").hide()
+        $("#good-job").hide()
+        $("#not-quite").hide()
         $(".score").hide()
     }
 
@@ -52,6 +53,9 @@ $(document).ready(function () {
         $("#timer").show()
         $("#game-rules").hide()
         $("#start-button").hide()
+        $(".display-right-answer").hide()
+        $("#good-job").hide()
+        $("#not-quite").hide()
         
         $("#answer-one").text(`a) ${trivia[currentQuestion].answers[0]}`)
         $("#answer-two").text(`b) ${trivia[currentQuestion].answers[1]}`)
@@ -59,10 +63,76 @@ $(document).ready(function () {
         $("#answer-four").text(`d) ${trivia[currentQuestion].answers[3]}`)
     }
 
+    function generateTimer () {
+
+        function run() {
+            clearInterval(intervalId);
+            intervalId = setInterval(decrement, 1000);
+            $("#seconds").text(counter);
+        }
+      
+        function decrement() {
+            counter--;
+            if (counter === 0) {
+                clearInterval(intervalId);
+                unAnswered++
+                currentQuestion++
+                $(".answer").hide()
+                $(".display-right-answer").show()
+                $("#timer").hide()
+                setTimeout(questionGenerator, 4000);
+            }
+            $("#seconds").text(counter);
+        }
+
+        run ()
+
+    }
+
+    $(".answer").on("click", captureUserAnswer);
+
+    function captureUserAnswer () {
+        clearInterval(intervalId);
+        // questionChecker();
+    
+        if (gameStart === false) {
+          return false;
+        }
+    
+        let answerIndex = parseInt($(this).attr("value"));
+    
+        if (answerIndex === trivia[currentQuestion].rightAnswer) {
+            correctAnswers++;
+            rightAnswer ()
+            console.log(correctAnswers);
+        } else {
+            incorrectAnswers++;
+            wrongAnswer ()
+            console.log(incorrectAnswers);
+        }
+    }
+
+
+    function rightAnswer () {
+        currentQuestion++
+        $(".answer").hide()
+        $(".display-right-answer").hide()
+        $("#good-job").show().text("Good Job!")
+        $("#timer").hide()
+
+        setTimeout(questionGenerator, 4000);
+    }
+
+    function wrongAnswer () {
+        currentQuestion++
+        $(".answer").hide()
+        $(".display-right-answer").show()
+        $("#good-job").hide()
+        $("#not-quite").show().text("Not quite!")
+        $("#timer").hide()
+
+        setTimeout(questionGenerator, 4000);
+    }
+
+
 });
-
-
-
-
-
-// $("<div>").text(trivia.questionOne.question).appendTo("#question")
